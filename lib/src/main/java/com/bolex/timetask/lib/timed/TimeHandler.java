a1fc6ec057fb71dc443906d3d8f1caa9a9d97a3d
liushenen <305853782@qq.com>
2017-11-10 17:05:42 +0800

    re

--- lib/src/main/java/com/bolex/timetask/lib/timed/TimeHandler.java
+++ lib/src/main/java/com/bolex/timetask/lib/timed/TimeHandler.java
@@ -5,7 +5,7 @@ package com.bolex.timetask.lib.timed;
  */
 
 public interface TimeHandler<T extends Task> {
-    void exeTask(T mTask);//当前执行
+    void exeTask(T mTask);//马上要执行
     void overdueTask(T mTask);//已过期
-    void futureTask(T mTask);//未来预约
+    void futureTask(T mTask);//未来会执行
 }
